0a491854c24a438c7f27e196f01892037bd5ff8d
Joana <joanajanefdez@gmail.com>
2022-08-01 23:28:53 +0200

    borrar imports unused

--- tarea2/src/main/java/UD20/tarea2/ex2.java
+++ tarea2/src/main/java/UD20/tarea2/ex2.java
@@ -1,13 +1,8 @@
 package UD20.tarea2;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -15,14 +10,15 @@ import java.awt.event.ActionEvent;
 public class ex2 extends JFrame {
 
 	private JPanel contentPane;
-	String botonPulsado = "";
-	JLabel lblNewLabel;
+	JLabel infoPulsadoLbl;
+	JButton Boton1;
+	JButton Boton2;
 
 	/**
 	 * Create the frame.
 	 */
 	public ex2() {
-		
+		//config ventana
 		setTitle("Ventana con interaccion simple");
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,26 +27,30 @@ public class ex2 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 				
-		JButton Boton1 = new JButton("Boton 1");		
+		//creacion boton1
+		Boton1 = new JButton("Boton 1");		
 		Boton1.setBounds(110, 41, 89, 23);
-		contentPane.add(Boton1);
+		contentPane.add(Boton1);				
 		
-		Boton1.addActionListener(accionClic);
-		
-		JButton Boton2 = new JButton("Boton 2");
+		//creacion boton2
+		Boton2 = new JButton("Boton 2");
 		Boton2.setBounds(275, 41, 89, 23);
-		contentPane.add(Boton2);
+		contentPane.add(Boton2);		
 		
+		//añadimos actionListener
+		Boton1.addActionListener(accionClic);
 		Boton2.addActionListener(accionClic);
 		
-		lblNewLabel = new JLabel("Has pulsado: ");
-		lblNewLabel.setBounds(25, 118, 129, 56);
-		contentPane.add(lblNewLabel);
+		//creacion etiqueta
+		infoPulsadoLbl = new JLabel("Has pulsado: ");
+		infoPulsadoLbl.setBounds(25, 118, 129, 56);
+		contentPane.add(infoPulsadoLbl);
 	}
 	
 	ActionListener accionClic = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-			lblNewLabel.setText("Has pulsado: "+e.getActionCommand());			
+			//sobreescribimos texto etiqueta
+			infoPulsadoLbl.setText("Has pulsado: "+e.getActionCommand());			
 		}
 	};
 }
