9aca1be0591822aa90955fda04cf6243249d1046
Joana <joanajanefdez@gmail.com>
2022-08-01 22:54:25 +0200

    ex1

--- tareasUD20.1/src/main/java/ud20/tareas/App.java
+++ tareasUD20.1/src/main/java/ud20/tareas/App.java
@@ -1,13 +1,22 @@
 package ud20.tareas;
 
+import java.awt.EventQueue;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App{
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ex1 frame = new ex1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
