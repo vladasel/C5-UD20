f58bad97d94daf6407b383fbadc4f78f4dce7035
ionela vladasel <ionelavladasel2710@gmail.com>
2022-08-01 13:23:22 +0200

    created buttons in task9

--- tarea9/src/main/java/UD20/tarea9/App.java
+++ tarea9/src/main/java/UD20/tarea9/App.java
@@ -1,13 +1,22 @@
 package UD20.tarea9;
 
+import java.awt.EventQueue;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Juego_de_memoria frame = new Juego_de_memoria();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
---
+++ tarea9/src/main/java/UD20/tarea9/Juego_de_memoria.java
@@ -0,0 +1,58 @@
+package UD20.tarea9;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JToggleButton;
+
+public class Juego_de_memoria extends JFrame {
+
+	private JPanel contentPane;
+	JToggleButton primerBoton = null;
+	JToggleButton[] arrayBotones;
+
+	/**
+	 * Create the frame.
+	 */
+	public Juego_de_memoria() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(4, 4, 0, 0));
+
+		// crear acccion
+		ActionListener actList = new ActionListener() {
+			public void actionPerformed(ActionEvent clickBoton) {
+				// objeto tipo toggleButon, se utiliza para sacar el botón que a lanzado el
+				// action
+				JToggleButton boton = (JToggleButton) clickBoton.getSource();// funciona bien solo si todos los botones
+																				// son
+				// Del mismo tipo, en caso de mas tipos de botones, se crearían mas actions
+
+				if (primerBoton == null) {
+					primerBoton = boton;
+				} else {
+					// comprobar colores
+				}
+			}
+		};
+
+		// Crear array de togglebuttons
+		arrayBotones = new JToggleButton[16];
+		// rellenar el array
+		for (int i = 0; i < arrayBotones.length; i++) {
+			arrayBotones[i] = new JToggleButton(Integer.toString(i + 1));
+			contentPane.add(arrayBotones[i]);
+		}
+
+	}
+}
