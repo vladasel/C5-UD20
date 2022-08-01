1ea093ea8b238ebb783417b34809a9c7082a55dc
IONELA VLADASEL <108732298+vladasel@users.noreply.github.com>
2022-08-01 12:38:01 +0200

    created tarea9 directory

---
+++ tarea9/src/main/java/UD20/tarea9/App.java
@@ -0,0 +1,13 @@
+package UD20.tarea9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
